56f2407d5247efc99d93f656326c5580d59838af
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2020-11-02 00:06:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+var ground;
+var b1,b2,b3;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	ball = new Paper(100,100,20);
+	b1 =new Dustbin(600,340,10,40)
+	b2 =new Dustbin(750,340,10,40)
+	b3=new Dustbin(675,360,150,10)
+	ground = new Ground(400,370,800,10);
+	Engine.run(engine);
+  
+}
+
+//function draw
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ball.display();
+  b3.display();
+  b1.display();
+  b2.display();
+  
+  ground.display();
+
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:43,y:-40});
+	}
+}
+
