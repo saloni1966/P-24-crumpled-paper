949e9fbd9932dea98c55d505101ecc161949bbd5
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2020-11-01 23:33:38 +0530

    first commit

---
+++ paper.js
@@ -0,0 +1,40 @@
+class Paper{
+    constructor(x,y,r){
+
+        var options={
+            isStatic: false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+
+        }
+        
+        this.body= Bodies.circle(x,y,30,options);
+        this.r = 30;
+        World.add(world,this.body);
+
+
+    }
+    display(){
+
+        var pos = this.body.position;
+        push()
+			translate(pos.x, pos.y);
+			rectMode(CENTER)
+			
+            fill(255,0,255)
+            ellipseMode(CENTER);
+			ellipse(0,0,this.r, this.r);
+			pop()
+       
+
+
+    }
+
+
+
+
+
+
+
+}
